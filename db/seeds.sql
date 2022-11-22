86aef2330dd83ecae9615cb68ca2aa26016a37d2
OVOMARK <mcncstatefan@icloud.com>
2022-11-22 13:40:09 -0500

    updated logic for seeding as well as testing db querys

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,13 @@
+USE employee_db;
+
 INSERT INTO department (id, name)
 VALUES  (1, "Marcus"),
         (2, "Kaitlin");
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES  (10, "Sales Lead", 100000.50, 1),
-        (11, "Software Engineer", 90000.50, 2);
+VALUES  (1, "Sales Lead", 100000.50, 1),
+        (2, "Software Engineer", 90000.50, 2);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES  (20, "Marcus", "Carlough", 11, 21),
-        (21, "Kaitlin", "Carlough", 10, 20);
+VALUES  (1, "Marcus", "Carlough", 1, null),
+        (2, "Kaitlin", "Carlough", 2, null);
